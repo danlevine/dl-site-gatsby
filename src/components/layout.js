595b9b5c4290fc46904bf2e8d7f37e24fceae377
Daniel Levine <dlevine210@gmail.com>
2018-09-21 23:23:07 -0500

    Remove boilerplate inline CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -45,16 +45,7 @@ const Layout = ({ children }) => (
           <html lang="en" />
         </Helmet>
         <AppHeader links={navLinks} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        {children}
       </>
     )}
   />
